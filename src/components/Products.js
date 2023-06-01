31a73424390df50e1e49edaa6a11200fbfb2ff39
Nathan <namo678@me.com>
2023-06-01 08:15:38 -0400

    products page connected to API

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,48 @@
-import React from "react";
-import { Button, Col, Form, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, CardGroup, Col, Form, Row } from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import cookiepic from '../2ChocolateChipCookies.jpeg'
 
 const Products = () => {
+
+    const [cookies, setCookies] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3000/products').then(res => res.json())
+        .then((data) => {
+            setCookies(data)
+        })
+    },[])
+
+    const cookieCards = cookies.map((cookie) => {
+        return (
+            <Col key={cookie.id} style={{marginBottom: '1%'}}>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={cookiepic} />
+                    <Card.Body>
+                        <Card.Title>{cookie.name}</Card.Title>
+                        <Card.Text>
+                        {cookie.description}
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroup.Item>${cookie.price}</ListGroup.Item>
+                        <ListGroup.Item>{cookie.box_count} cookies per box</ListGroup.Item>
+                        <ListGroup.Item>???</ListGroup.Item>
+                    </ListGroup>
+                    <Card.Body>
+                        <form>
+                            <Form.Label>Quantity</Form.Label>
+                            <Form.Control type="number" placeholder={0} min={0} style={{width: '40%'}}/>
+                            <Button type="submit">Add to Cart</Button>
+                        </form>
+                    </Card.Body>
+                </Card>   
+            </Col>)
+    })
+
     return (
         <>
             <h1>View and Order Products</h1>
@@ -16,36 +53,11 @@ const Products = () => {
             <br/>
             <br/>
             <h1>Products with 'add to cart' button</h1>
-            <Row xs={1} md={3} className="g-4">
-                {
-                    Array.from({length: 4}).map((_, idx) => {
-                        return (
-                        <Col key={idx}>
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={cookiepic} />
-                                <Card.Body>
-                                    <Card.Title>Cookie name</Card.Title>
-                                    <Card.Text>
-                                    cookie description
-                                    </Card.Text>
-                                </Card.Body>
-                                <ListGroup className="list-group-flush">
-                                    <ListGroup.Item>price?</ListGroup.Item>
-                                    <ListGroup.Item>Quantitiy?</ListGroup.Item>
-                                    <ListGroup.Item>???</ListGroup.Item>
-                                </ListGroup>
-                                <Card.Body>
-                                    <form>
-                                        <Form.Label>Quantitiy</Form.Label>
-                                        <Form.Control type="number" placeholder={0} min={0} style={{width: '40%'}}/>
-                                        <Button type="submit">Add to Cart</Button>
-                                    </form>
-                                </Card.Body>
-                            </Card>   
-                        </Col>)
-                    })
-                }
-                </Row>
+            <CardGroup style={{margin: '10%'}}>
+                {/* <Row xs={1} md={3} className="g-4"> */}
+                    {cookieCards}
+                {/* </Row> */}
+            </CardGroup>
         </>
     )
 }
